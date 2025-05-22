6090cb4ba8908b3d8598c562f0058bdf74b4922e
AldaneTech <github.ether823@passmail.net>
2025-05-22 19:05:03 +0200

    chore: externalize JWT secret key using application properties

--- src/main/java/net/aldane/cash_balance/config/jwt/JwtUtil.java
+++ src/main/java/net/aldane/cash_balance/config/jwt/JwtUtil.java
@@ -3,6 +3,7 @@ package net.aldane.cash_balance.config.jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -13,7 +14,14 @@ import java.util.Date;
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
-    private final SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
